39dd141e4298dd943a7f01e6b7672d1ce0cc5898
chris-connoll-y <89544126+chris-connoll-y@users.noreply.github.com>
2021-09-02 09:42:13 -0700

    Update ATM1Tester.java

--- src/ATM1Tester.java
+++ src/ATM1Tester.java
@@ -3,7 +3,7 @@ public class ATM1Tester {
 	public static void main (String [] args) {
 		ATM bank = new ATM ();
 		System.out.println (bank.deposit("monke", 10));
-		System.out.println(bank.withdraw("monk", 5));
+		System.out.println(bank.withdraw("monke", 15));
 		System.out.println(bank.checkBalance("monke"));
 	}
 }
